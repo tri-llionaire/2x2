dc6bc9655e8a39316845e65bb2dda07ab2b34680
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2021-01-25 16:29:18 -0500

    Update movedictionary.py

--- movedictionary.py
+++ movedictionary.py
@@ -1,3 +1,4 @@
+import random, itertools
 def or_L(current):
     new = list(current)
     new[2] = current[20]
@@ -269,3 +270,173 @@ def move_Fd(current):
     new[11] = current[9]
     new[12] = current[10]
     return new
+def rot_x(current):
+    new = move_R(current)
+    new = or_Lp(new)
+    return new
+def rot_xp(current):
+    new = move_Rp(current)
+    new = or_L(new)
+    return new
+def rot_xd(current):
+    new = move_Rd(current)
+    new = or_Ld(new)
+    return new
+def rot_y(current):
+    new = move_U(current)
+    new = or_Dp(new)
+    return new
+def rot_yp(current):
+    new = move_Up(current)
+    new = or_D(new)
+    return new
+def rot_yd(current):
+    new = move_Ud(current)
+    new = or_Dd(new)
+    return new
+def rot_z(current):
+    new = move_F(current)
+    new = or_Bp(new)
+    return new
+def rot_zp(current):
+    new = move_Fp(current)
+    new = or_B(new)
+    return new
+def rot_zd(current):
+    new = move_Fd(current)
+    new = or_Bd(new)
+    return new
+def scramble(current, entered_scramble):
+    current[25] = entered_scramble
+    for x in entered_scramble.split():
+        if x == 'R':
+            current = move_R(current)
+        elif x == 'R\'':
+            current = move_Rp(current)
+        elif x == 'R2':
+            current = move_Rd(current)
+        elif x == 'U':
+            current = move_U(current)
+        elif x == 'U\'':
+            current = move_Up(current)
+        elif x == 'U2':
+            current = move_Ud(current)
+        elif x == 'F':
+            current = move_F(current)
+        elif x == 'F\'':
+            current = move_Fp(current)
+        elif x == 'F2':
+            current = move_Fd(current)
+        elif x == 'L':
+            current = or_L(current)
+        elif x == 'L\'':
+            current = or_Lp(current)
+        elif x == 'L2':
+            current = or_Ld(current)
+        elif x == 'D':
+            current = or_D(current)
+        elif x == 'D\'':
+            current = or_Dp(current)
+        elif x == 'D2':
+            current = or_Dd(current)
+        elif x == 'B':
+            current = or_B(current)
+        elif x == 'B\'':
+            current = or_Bp(current)
+        elif x == 'B2':
+            current = or_Bd(current)
+        elif x == 'x':
+            current = rot_x(current)
+        elif x == 'x\'':
+            current = rot_xp(current)
+        elif x == 'x2':
+            current = rot_xd(current)
+        elif x == 'y':
+            current = rot_y(current)
+        elif x == 'y\'':
+            current = rot_yp(current)
+        elif x == 'y2':
+            current = rot_yd(current)
+        elif x == 'z':
+            current = rot_z(current)
+        elif x == 'z\'':
+            current = rot_zp(current)
+        elif x == 'z2':
+            current = rot_zd(current)
+        else:
+            pass
+    return current
+def issolved(status):
+    if (status[1] == status[2] == status[3] == status[4]) and (status[5] == status[6] == status[7] == status[8]) and (status[9] == status[10] == status[11] == status[12]) and (status[13] == status[14] == status[15] == status[16]) and (status[17] == status[18] == status[19] == status[20]) and (status[21] == status[22] == status[23] == status[24]):
+        return True
+    else:
+        return False
+def guesses(repeating):
+    was = ''; todo = []; bad = 0; moves = ['R', 'R\'', 'R2', 'U', 'U\'', 'U2', 'F', 'F\'', 'F2']
+    for x in itertools.product(moves, repeat=repeating):
+        for i in x:
+            if i[0] == was:
+                bad = 1
+            was = i[0]
+        if bad == 0:
+            todo.append(' '.join(x))
+            was = ''
+        bad = 0
+    return todo
+def output(scrambled):
+    print('     {} {}\n     {} {}\n{} {}  {} {}  {} {}  {} {}\n{} {}  {} {}  {} {}  {} {}\n     {} {}\n     {} {}'.format(scrambled[2], scrambled[1], scrambled[3], scrambled[4], scrambled[6], scrambled[5], scrambled[10], scrambled[9], scrambled[14], scrambled[13], scrambled[18], scrambled[17], scrambled[7], scrambled[8], scrambled[11], scrambled[12], scrambled[15], scrambled[16], scrambled[19], scrambled[20], scrambled[22], scrambled[21], scrambled[23], scrambled[24]))
+def remove(string):
+    string = string.split()
+    last = '0'
+    h = 0
+    for x in string:
+        if last[0] == x[0]:
+            h = 1
+        last = x
+    if h == 1:
+        return True
+    else:
+        return False
+def generate():
+    last = 'i'; scramble = ''; moves = ['R', 'R\'', 'R2', 'U', 'U\'', 'U2', 'F', 'F\'', 'F2']
+    for i in 'twelveletter':
+        index = random.randint(0, 8)
+        while last[0] == moves[index][0]:
+            index = random.randint(0, 8)
+        scramble = scramble + moves[index] + ' '
+        last = moves[index]
+    return scramble
+def find(u):
+    if u[1:5] == ['w', 'w', 'w', 'w']:
+        return 'white'
+    elif u[5:9] == ['o', 'o', 'o', 'o']:
+        return 'orange'
+    elif u[9:13] == ['g', 'g', 'g', 'g']:
+        return 'green'
+    elif u[13:17] == ['r', 'r', 'r', 'r']:
+        return 'red'
+    elif u[17:21] == ['b', 'b', 'b', 'b']:
+        return 'blue'
+    elif u[21:25] == ['y', 'y', 'y', 'y']:
+        return 'yellow'
+    else:
+        return ''
+def reverse(v):
+    newv = ''
+    vs = v[25].split()
+    for i in vs[::-1]:
+        if i == 'R':
+            newv = newv + 'R\' '
+        elif i == 'U':
+            newv = newv + 'U\' '
+        elif i == 'F':
+            newv = newv + 'F\' '
+        elif i == 'R\'':
+            newv = newv + 'R '
+        elif i == 'U\'':
+            newv = newv + 'U '
+        elif i == 'F\'':
+            newv = newv + 'F '
+        else:
+            newv = newv + i + ' '
+    return newv
