2729e0bf680a48ef1a190c0b5db4809bc1605716
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2021-01-27 15:34:10 -0500

    Update tiny.py

--- tiny.py
+++ tiny.py
@@ -1,4 +1,4 @@
-import itertools;s='wwwwooooggggrrrrbbbbyyyy';q=1;c=y(s, input(': '))
+import itertools;s='wwwwooooggggrrrrbbbbyyyy';q=1
 def z(r):
     w='';t=[];b=0;m=['R','R\'','R2','U','U\'','U2','F','F\'','F2']
     for x in itertools.product(m,repeat=r):
@@ -17,7 +17,7 @@ def U(c):
     for i in range(24):n[i]=c[int(u[(2*i):(2*i)+2])]
     return ''.join(n)
 def F(c):
-    f = '000107042105062009101108120203151617181913142223';n=list(c)
+    f='000107042105062009101108120203151617181913142223';n=list(c)
     for i in range(24):n[i]=c[int(f[(2*i):(2*i)+2])]
     return ''.join(n)
 def y(c,e):
@@ -35,7 +35,7 @@ def y(c,e):
     return c
 def w(v):
     n = ''
-    for i in v[::-1]:
+    for i in v[24:].split()[::-1]:
         if i=='R':n=n+'R\' '
         elif i=='U':n=n+'U\' '
         elif i=='F':n=n+'F\' '
@@ -44,10 +44,57 @@ def w(v):
         elif i=='F\'':n=n+'F '
         else:n=n+i+' '
     return n
+def r(s):
+    s = s.split()
+    for x in range(6):
+        try:
+            if s[x][0] == s[x+1][0]:
+                a = s[x]; b = s[x+1];del s[x+1]
+                if a=='R':
+                    if b=='R':s[x]='R2'
+                    elif b=='R\'':del s[x]
+                    else:s[x]='R\''
+                elif a=='U':
+                    if b=='U':s[x]='U2'
+                    elif b=='U\'':del s[x]
+                    else:s[x]='U\''
+                elif a=='F':
+                    if b=='F':s[x]='F2'
+                    elif b=='F\'':del s[x]
+                    else:s[x]='F\''
+                elif a=='R\'':
+                    if b=='R':del s[x]
+                    elif b=='R\'':s[x]='R2'
+                    else:s[x]='R'
+                elif a=='U\'':
+                    if b=='U2':s[x]='U'
+                    elif b=='U':del s[x]
+                    else:s[x]='U2'
+                elif a=='F\'':
+                    if b=='F\'':s[x]='F2'
+                    elif b=='F2':s[x]='F'
+                    else:del s[x]
+                elif a=='R2':
+                    if b=='R':s[x]='R\''
+                    elif b=='R2':del s[x]
+                    else:s[x]='R'
+                elif a=='U2':
+                    if b=='U':s[x]='U\''
+                    elif b=='U2':del s[x]
+                    else:s[x]='U'
+                else:
+                    if b=='F\'':s[x]='F'
+                    elif b=='F2':del s[x]
+                    else:s[x]='F'
+        except:pass
+    return ' '.join(s)
+c=y(s, input(': '))
 while q!=0:
-    f=[];g=[]
+    f=[];g=[];o=[]
     for l in z(q):f.append(y(c[:24], l));g.append(y(s, l))
     for u in f:
         for v in g:
-            if u[:24]==v[:24]:print(u[24:]+' '+w(v[24:].split()));q=-1
+            if u[:24]==v[:24]:
+                k=r(u[24:]+' '+w(v))
+                if k not in o:print(k);o.append(k);q=-1
     q+=1
