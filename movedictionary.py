529f6a35250174e5554a41389f103ee456f34129
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2021-01-28 15:09:26 -0500

    Update movedictionary.py

--- movedictionary.py
+++ movedictionary.py
@@ -1,210 +1,67 @@
 import random, itertools
-def or_L(current):
-    lmove = '001916030506070408010211121314152217182120091023'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(lmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_Lp(current):
-    lpmove = '000910030704050608212211121314150217180120191623'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(lpmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_Ld(current):
-    ldmove = '002122030607040508191611121314151017180920010223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(ldmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_D(current):
-    dmove = '000102030405181908090607121310111617141521222320'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(dmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_Dp(current):
-    dpmove = '000102030405101108091415121318191617060723202122'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(dpmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_Dd(current):
-    ddmove = '000102030405141508091819121306071617101122232021'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(ddmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_B(current):
-    bmove = '151202030400010708091011231314221718191620210506'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(bmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_Bp(current):
-    bpmove = '050602030423220708091011011314001916171820211512'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(bpmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def or_Bd(current):
-    bdmove = '222302030415120708091011061314051819161720210001'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(bdmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_R(current):
-    rmove = '080102110405060720091023131415121603001918212217'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(rmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_Rp(current):
-    rpmove = '180102170405060700091003151213141623201908212211'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(rpmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_Rd(current):
-    rdmove = '200102230405060718091017141512131611081900212203'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(rdmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_U(current):
-    umove = '010203000809060712131011161714150405181920212223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(umove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_Up(current):
-    upmove = '030001021617060704051011080914151213181920212223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(upmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_Ud(current):
-    udmove = '020300011213060716171011040514150809181920212223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(udmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_F(current):
-    fmove = '000107042105062009101108120203151617181913142223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(fmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_Fp(current):
-    fpmove = '000113140305060211080910122021151617181907042223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(fpmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def move_Fd(current):
-    fdmove = '000120211405061310110809120704151617181902032223'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(fdmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def rot_x(current):
-    xmove = '080910110704050620212223131415120203000118191617'
-    new = list(current)
-    for i in range(24):
-        new[i] = current[int(xmove[(2*i):(2*i)+2])]
-    return ''.join(new)
-def rot_xp(current):
-    new = move_Rp(current)
-    new = or_L(new)
-    return new
-def rot_xd(current):
-    new = move_Rd(current)
-    new = or_Ld(new)
-    return new
-def rot_y(current):
-    new = move_U(current)
-    new = or_Dp(new)
-    return new
-def rot_yp(current):
-    new = move_Up(current)
-    new = or_D(new)
-    return new
-def rot_yd(current):
-    new = move_Ud(current)
-    new = or_Dd(new)
-    return new
-def rot_z(current):
-    new = move_F(current)
-    new = or_Bp(new)
-    return new
-def rot_zp(current):
-    new = move_Fp(current)
-    new = or_B(new)
-    return new
-def rot_zd(current):
-    new = move_Fd(current)
-    new = or_Bd(new)
-    return new
 def scramble(current, entered_scramble):
-    current = current + entered_scramble
     for x in entered_scramble.split():
         if x == 'R':
-            current = move_R(current)
+            move = '080102110405060720091023131415121603001918212217'
         elif x == 'R\'':
-            current = move_Rp(current)
+            move = '180102170405060700091003151213141623201908212211'
         elif x == 'R2':
-            current = move_Rd(current)
+            move = '200102230405060718091017141512131611081900212203'
         elif x == 'U':
-            current = move_U(current)
+            move = '010203000809060712131011161714150405181920212223'
         elif x == 'U\'':
-            current = move_Up(current)
+            move = '030001021617060704051011080914151213181920212223'
         elif x == 'U2':
-            current = move_Ud(current)
+            move = '020300011213060716171011040514150809181920212223'
         elif x == 'F':
-            current = move_F(current)
+            move = '000107042105062009101108120203151617181913142223'
         elif x == 'F\'':
-            current = move_Fp(current)
+            move = '000113140305060211080910122021151617181907042223'
         elif x == 'F2':
-            current = move_Fd(current)
+            move = '000120211405061310110809120704151617181902032223'
         elif x == 'L':
-            current = or_L(current)
+            move = '001916030506070408010211121314152217182120091023'
         elif x == 'L\'':
-            current = or_Lp(current)
+            move = '000910030704050608212211121314150217180120191623'
         elif x == 'L2':
-            current = or_Ld(current)
+            move = '002122030607040508191611121314151017180920010223'
         elif x == 'D':
-            current = or_D(current)
+            move = '000102030405181908090607121310111617141521222320'
         elif x == 'D\'':
-            current = or_Dp(current)
+            move = '000102030405101108091415121318191617060723202122'
         elif x == 'D2':
-            current = or_Dd(current)
+            move = '000102030405141508091819121306071617101122232021'
         elif x == 'B':
-            current = or_B(current)
+            move = '151202030400010708091011231314221718191620210506'
         elif x == 'B\'':
-            current = or_Bp(current)
+            move = '050602030422230708091011011314001916171820211512'
         elif x == 'B2':
-            current = or_Bd(current)
+            move = '222302030415120708091011061314051819161720210001'
         elif x == 'x':
-            current = rot_x(current)
+            move = '080910110704050620212223131415120203000118191617'
         elif x == 'x\'':
-            current = rot_xp(current)
+            move = '181916170506070400010203151213142223202108091011'
         elif x == 'x2':
-            current = rot_xd(current)
+            move = '202122230607040518191617141512131011080900010203'
         elif x == 'y':
-            current = rot_y(current)
+            move = '010203000809101112131415161718190405060723202122'
         elif x == 'y\'':
-            current = rot_yp(current)
+            move = '030001021617181904050607080910111213141521222320'
         elif x == 'y2':
-            current = rot_yd(current)
+            move = '020300011213141516171819040506070809101122232021'
         elif x == 'z':
-            current = rot_z(current)
+            move = '050607042122232009101108010203001916171813141512'
         elif x == 'z\'':
-            current = rot_zp(current)
+            move = '151213140300010211080910232021221718191607040506'
         elif x == 'z2':
-            current = rot_zd(current)
+            move = '222320211415121310110809060704051819161702030001'
         else:
             pass
-    return current
+        new = list(current)
+        for i in range(24):
+            new[i] = current[int(move[(2*i):(2*i)+2])]
+        current = ''.join(new)
+    return current + entered_scramble
 def issolved(status):
     if (status[0] == status[1] == status[2] == status[3]) and (status[4] == status[5] == status[6] == status[7]) and (status[8] == status[9] == status[10] == status[11]) and (status[12] == status[13] == status[14] == status[15]) and (status[16] == status[17] == status[18] == status[19]) and (status[20] == status[21] == status[22] == status[23]):
         return True
@@ -307,17 +164,17 @@ def generate():
         last = moves[index]
     return scramble
 def find(u):
-    if u[:4] == ['w', 'w', 'w', 'w']:
+    if u[:4] == 'wwww':
         return 'white'
-    elif u[4:8] == ['o', 'o', 'o', 'o']:
+    elif u[4:8] == 'oooo':
         return 'orange'
-    elif u[8:12] == ['g', 'g', 'g', 'g']:
+    elif u[8:12] == 'gggg':
         return 'green'
-    elif u[12:16] == ['r', 'r', 'r', 'r']:
+    elif u[12:16] == 'rrrr':
         return 'red'
-    elif u[16:20] == ['b', 'b', 'b', 'b']:
+    elif u[16:20] == 'bbbb':
         return 'blue'
-    elif u[20:24] == ['y', 'y', 'y', 'y']:
+    elif u[20:24] == 'yyyy':
         return 'yellow'
     else:
         return ''
